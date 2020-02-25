cd80ac39e1a1c2496a6869c3ad8c59fb42e04040
andrewli <andrew@liprogramming.com>
2020-02-24 20:49:00 -0500

    added distinct to getting horoscope query

--- src/main/java/com/liprogramming/horoscopeAPI/Dao/HoroscopeDao.java
+++ src/main/java/com/liprogramming/horoscopeAPI/Dao/HoroscopeDao.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface HoroscopeDao extends JpaRepository<Horoscope, Long> {
 
-    @Query("SELECT h FROM Horoscope h WHERE zodiacId = ?1 AND created = ?2")
+    @Query("SELECT DISTINCT h FROM Horoscope h WHERE zodiacId = ?1 AND created = ?2")
     public Horoscope getToday(Integer zodiacId, LocalDate date);
 
     public List<Horoscope> getAllByCreatedEquals(LocalDate today);
